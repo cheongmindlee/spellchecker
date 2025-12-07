5f44d432e2fa03c4a9e515501c12233e84594ba2
reocraft <reocraft.creeper@gmail.com>
2025-12-07 17:12:41 -0600

    Implemented add and isWord

--- src/main/java/edu/grinnell/csc207/spellchecker/SpellChecker.java
+++ src/main/java/edu/grinnell/csc207/spellchecker/SpellChecker.java
@@ -3,6 +3,7 @@ package edu.grinnell.csc207.spellchecker;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -29,49 +30,102 @@ public class SpellChecker {
         private char letter;
         private boolean isWord;
         private List<Node> seq;
-        public Node(char letter, boolean isWord){
+
+        public Node(char letter, boolean isWord) {
             this.letter = letter;
             this.isWord = isWord;
-            seq = null;
+            seq = new ArrayList<>();
         }
-
 
     }
 
     /** The root of the SpellChecker */
-    private Node root;
+    private Node root = new Node(' ', false);
 
     public SpellChecker(List<String> dict) {
-        //
-    }
-
-    public void add(String word) {
-        Node temp = root;
-        int iter = 0;
-        for (char c : word.toCharArray()) {
-            for(Node node: temp.seq){
-                boolean exists = false;
-                if(node.letter == c){
-                    temp = node;
-                    exists = true;
-                }
-
-                if (!exists) {
-                    //If we can't find the character add it
-                    if(iter == word.length() - 1){
-                        temp.seq.add(new Node(c, true));
-                    } else {
-                        temp.seq.add(new Node(c, false));
-                    }
-                }
-
-            }
-            iter++;
+        for (String s : dict) {
+            add(s);
         }
     }
 
+    public void add(String word) {
+
+        Node tree = root;
+        // if (tree == null) {
+        // tree = new Node(' ', false);
+        // }
+        for (int i = 0; i < word.length(); i++) {
+            boolean exists = false;
+            for (Node t : tree.seq) {
+                if (t.letter == word.charAt(i)) {
+                    tree = t;
+                    exists = true;
+                    break;
+                }
+            }
+            if (!exists) {
+                if (i == word.length() - 1) {
+                    Node t = new Node(word.charAt(i), true);
+                    tree.seq.add(t);
+                    tree = t;
+                    // tree = tree.seq.get(tree.seq.size() - 1);
+
+                } else {
+                    Node t = new Node(word.charAt(i), false);
+                    tree.seq.add(t);
+                    tree = t;
+                    // tree = tree.seq.get(tree.seq.size() - 1);
+                }
+            }
+        }
+
+        // Node temp = root;
+        // int iter = 0;
+        // for (char c : word.toCharArray()) {
+        // for(Node n : temp.seq){
+        // boolean exists = false;
+        // if(n.letter == c){
+        // temp = n;
+        // exists = true;
+        // }
+
+        // if (!exists) {
+        // //If we can't find the character add it
+        // if(iter == word.length() - 1){
+        // temp.seq.add(new Node(c, true));
+        // } else {
+        // temp.seq.add(new Node(c, false));
+        // }
+        // }
+
+        // }
+        // iter++;
+        // }
+    }
+
     public boolean isWord(String word) {
-        // TODO: implement me!
+
+        Node tree = root;
+        if (tree == null) {
+            return false;
+        }
+
+        for (int i = 0; i < word.length(); i++) {
+            boolean exists = false;
+            for (Node t : tree.seq) {
+                if (t.letter == word.charAt(i)) {
+                    exists = true; // If issue arises delete this
+                    if (i == word.length() - 1) {
+                        return t.isWord;
+                    }
+                    tree = t;
+                    break;
+                }
+            }
+            if (!exists) {
+                return false; // and delete that
+            }
+        }
         return false;
     }
 
